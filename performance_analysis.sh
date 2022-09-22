5daa28dda407bfee0e1fb1cba6cfe14730b70841
Tim-Tadj <timtadj@gmail.com>
2022-09-22 11:21:25 +1000

    guvyt

--- performance_analysis.sh
+++ performance_analysis.sh
@@ -8,15 +8,53 @@
 # the relevant numbers, etc.).
 
 # get cli argument for number of iterations if it exists if it doesn't exist, set it to 20000
-if [ -z "$1" ]; then
+
+
+if [ -z $1 ];
+then
     iterations=20000
 else
     iterations=$1
 fi
 
+if [ -z $2 ];
+then
+    x=0
+else
+    x=$2
+fi
+
+if [ -n $3 ];
+then
+    y=0
+else
+    y=$3
+fi
+
+if [ -n $4 ];
+then
+    size=2
+else
+    size=$4
+fi
+
+if [ -n $5 ];
+then
+    threads=2
+else
+    threads=$5
+fi
+
+if [ -n $6 ];
+then
+    ipc='s'
+else
+    ipc=$6
+fi
+
 # run make all print to file and print error if it fails
 echo "Running make all..."
-make all > make_all_output.rtf 2>&1 || { cat make_all_output.rtf ; rm make_all_output.rtf; exit 1; }
+make all > make_all_output.txt 2>&1 || { cat make_all_output.txt ; exit 1; }
 echo "Make all successful!"
 
 
@@ -29,25 +67,25 @@ do
     # print the program name
     echo $program
 
-
-
-
-    # get the time elapsed and memory used for each program
-    
-    /usr/bin/time -f "Time elapsed: %e seconds, Memory used: %M kilobytes" ./$program $iterations > /dev/null
+    # get the time elapsed and memory used for each program and print it to the terminal
+    # the default mandelbrot does not require a thread count
+    if [ $program == "mb5.out" ]; then
+        echo "Running $program with $iterations iterations, x origin = $x, $y y, $size size"
+        /usr/bin/time ./$program $iterations $x $y $size
+    # the fork program takes an extra argument specifying socket or pipe
+    # given as 's' and 'p' respectively
+    elif [ $program == "mb5_fork.out" ]; then
+        echo "Running $program with $iterations iterations, $x x, $y y, $size size, $threads threads, $ipc ipc"
+        /usr/bin/time ./$program $iterations $x $y $size $threads $ipc
+    else
+        echo "Running $program with $iterations iterations, $x x, $y y, $size size, $threads threads"
+        /usr/bin/time ./$program $iterations $x $y $size $threads
+    fi
     
     # plot the mandel.dat file using gnuplot and the mandel.gp file
     gnuplot mandel.gp > /dev/null
-
-    # make a directory for the images if it doesn't exist
-    mkdir -p images
-
-    # get the name of the program without the .out extension
-    program_name=$(echo $program | cut -d'.' -f1)
-
-    # move the image to the images directory
-    mv mandel.png images/$program_name.png
-
+    # save the plot as a png file
+    mv mandel.png $program.png 
     
 done
 
@@ -56,5 +94,3 @@ done
 
 # install gnuplot for wsl ubuntu
 # sudo apt-get install gnuplot
-
-# TODO analysis using Amdahl’s law
